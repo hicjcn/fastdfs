40dd263fc3b06fb0c03d6c89092b64b96ce1f136
chenjian <hi.cj@outlook.com>
2020-04-20 13:48:06 +0800

    feat:调整目录结构，新增springboot文件

---
+++ file-service/src/test/java/cn/jcera/fileservice/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jcera.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
